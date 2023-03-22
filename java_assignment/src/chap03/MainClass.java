b876412f3d4a3d53c5d9b2b1c77d3a5d62da09f9
kkang <rkdgus3617@naver.com>
2023-03-22 18:20:57 +0900

    chap3 과제풀이

---
+++ java_assignment/src/chap03/MainClass.java
@@ -0,0 +1,21 @@
+package chap03;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		MyCart cart = new MyCart(2000);
+		
+		Tv tv = new Tv();
+		Radio radio = new Radio();
+		Computer com = new Computer();
+		
+		cart.buy(tv);
+		cart.buy(radio);
+		cart.buy(com);
+		
+		
+
+	}
+
+}
